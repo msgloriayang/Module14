a15ba2149f7fd87b5377682d2887cdf51fdb1937
msgloriayang <yanggloria91@gmail.com>
2023-05-11 22:31:09 -0500

    changed secret property a second time

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
 const sess = {
-    secret: 'Psssst..',
+    secret: 'Psssst..issa secret',
     rolling: true,
     cookie: { maxAge: 300000 },
     resave: false,
