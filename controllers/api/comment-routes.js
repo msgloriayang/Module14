63ba8e3421e089eefc39de32988cac7e856950ad
msgloriayang <yanggloria91@gmail.com>
2023-05-12 00:35:22 -0500

    made changes to comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,86 +3,41 @@ const { Post, User, Comment } = require("../../models");
 const Auth = require("../../utils/auth");
 const sequelize = require('../../config/connection');
 
-router.get('/',(req,res)=>
-{
-    Comment.findAll(
-    {
-
-        }
-    )
-    .then(data=>res.json(data))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-
-router.get('/:id',(req,res)=>
-{
-    Comment.findOne(
-    {
-        where: 
-        {
-            id : req.params.id
-        }
-    })
-    .then(data=>res.json(data))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.post('/',(req,res)=>
-{
-    Comment.create(
-        {
-            comment_text: req.body.comment_text,
-            user_id:req.session.user_id,
-            post_id: req.body.post_id
-        })
-    .then(data=>res.json(data))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.put('/:id',(req,res)=>
-{
-    Comment.update(req.body,
-        {
-            where:
-            {
-                id: req.params.id
-            }
-        })
-    .then(data=>res.json(data))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.delete('/:id',(req,res)=>
-{
-    Comment.destroy({
-            where:
-            {
-                id: req.params.id
-            }
-        })
-    .then(data=>res.json(data))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-module.exports = router;
+//create a post
+router.post("/", async (req, res) => {
+    console.log("create comment")
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+      console.log(newComment)
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  //delete a post
+  router.delete("/:id",Auth, async (req, res) => {
+    try {
+      const postComment = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (commentData) {
+        res.status(200).json(commentData);
+      } else {
+        res.status(404).json({ message: "No comment found with this id!" });
+        return;
+      }
+  
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
