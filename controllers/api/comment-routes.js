a5a1163a05a7710a6929ef9caee4098f18730986
msgloriayang <yanggloria91@gmail.com>
2023-05-12 00:43:46 -0500

    fixed syntax error and removed extra router.post on comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,43 +1,48 @@
 const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
+const { Comment } = require("../../models");
 const Auth = require("../../utils/auth");
 const sequelize = require('../../config/connection');
 
-//create a post
-router.post("/", async (req, res) => {
-    console.log("create comment")
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+  });
+
+//Create a comment
+router.post("/", Auth, async (req, res) => {
     try {
       const newComment = await Comment.create({
         ...req.body,
         user_id: req.session.user_id,
       });
-      console.log(newComment)
+      console.log(newComment);
       res.status(200).json(newComment);
     } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  //delete a post
-  router.delete("/:id",Auth, async (req, res) => {
-    try {
-      const postComment = await Comment.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (commentData) {
-        res.status(200).json(commentData);
-      } else {
-        res.status(404).json({ message: "No comment found with this id!" });
-        return;
-      }
-  
-    } catch (err) {
+      console.log(err);
       res.status(500).json(err);
     }
   });
+    
+  //Delete a comment
+  router.delete("/:id", Auth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+          where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+          },
+        });
+    
+        if (commentData) {
+          res.status(200).json(commentData);
+        } else {
+          res.status(404).json({ message: "No comment found!" });
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });
   
   module.exports = router;
+  module.exports = sequelize;
