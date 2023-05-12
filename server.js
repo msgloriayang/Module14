301d91b816f059ef1b77ae5a5fbf001bd252e51a
msgloriayang <yanggloria91@gmail.com>
2023-05-11 22:22:17 -0500

    pushed recent changes to server.js

--- server.js
+++ server.js
@@ -2,32 +2,21 @@ const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
 const exsess = require('express-session');
-const sequel = require('connect-session-sequelize')(exsess.Store);
 
-const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
-// this is where the handlebars are
+// this is for the handlebars
 app.set('view engine', 'handlebars');
 app.engine('handlebars', handlebars({ helpers: helpers }));
-
-const sess = 
-{
-    secret: 'Super secret secret',
-    rolling: true,
-    cookie: {maxAge:300000},
-    resave: false,
-    saveUninitialized: true,
-    store: new sequel({db: sequelize})
-};
 
 app.use(routes);
 app.use(exsess(sess));
