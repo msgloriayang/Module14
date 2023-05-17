6da351822293205982e041abe4f4a2462a420a40
msgloriayang <yanggloria91@gmail.com>
2023-05-16 19:40:33 -0500

    added comment.js

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      commentText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "post",
+          key: "id",
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        timestamps: false,
+        modelName: 'comment'
+    }
+    );
+    
+    module.exports = Comment;
